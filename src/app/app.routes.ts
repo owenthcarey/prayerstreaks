39832cd6afa600c90e64a9cea8869db70ed31d9b
Owen Carey <37121709+owenthcarey@users.noreply.github.com>
2026-02-19 10:14:21 -0800

    refactor: move TabView to app component with per-tab page-router-outlets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,22 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
+import { TodayComponent } from './features/today/today.component';
+import { HistoryComponent } from './features/history/history.component';
+import { SettingsComponent } from './features/settings/settings.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: 'today',
+    component: TodayComponent,
+    outlet: 'todayTab',
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+    outlet: 'historyTab',
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    outlet: 'settingsTab',
+  },
 ];
