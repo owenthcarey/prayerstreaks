91a29fe35b305479613e7c5b3e911ac6e292fe08
Owen Carey <37121709+owenthcarey@users.noreply.github.com>
2026-02-20 11:56:24 -0800

    chore: set app identifier to com.prayerstreaks.app for store distribution

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.prayerstreaks',
+  id: 'com.prayerstreaks.app',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
